ca559bf27d06ff73d505b834964d3d9f5a46a56e
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-23 23:01:47 -0500

    3

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -7,12 +7,17 @@ var edad = 26;
 var estadoCivil = 'soltera';
 var arreglo = [nombre, apellido, cedula, edad, estadoCivil];
 function validarNumeroDeStringsEnArreglo (arreglo){
-    return (nombre + apellido + cedula + estadoCivil);
+    if (nombre == edad){
+        console.log(typeof edad)
+    }else{
+        console.log("No es string")
+    }
+    return arreglo;
 }
 console.log(typeof nombre) ;
 console.log(typeof apellido) ;
 console.log(typeof cedula) ;
 console.log(typeof edad) ;
 console.log(typeof estadoCivil);
-console.log(typeof arreglo);
-console.log(arreglo.length);
+//console.log(typeof arreglo);
+console.log('Numero de strings es: ',arreglo.length);
