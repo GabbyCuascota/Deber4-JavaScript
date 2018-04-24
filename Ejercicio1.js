359599fafc886fede3b3c9d1ae15d60fd45f83ab
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-23 22:50:37 -0500

    2

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,5 +1,18 @@
 // CUASCOTA GABRIELA
 // desarrollo
-function validarNumeroDeStringsEnArreglo (){
-
+var nombre = 'Gabriela';
+var apellido = 'Cuascota';
+var cedula = '1720437670';
+var edad = 26;
+var estadoCivil = 'soltera';
+var arreglo = [nombre, apellido, cedula, edad, estadoCivil];
+function validarNumeroDeStringsEnArreglo (arreglo){
+    return (nombre + apellido + cedula + estadoCivil);
 }
+console.log(typeof nombre) ;
+console.log(typeof apellido) ;
+console.log(typeof cedula) ;
+console.log(typeof edad) ;
+console.log(typeof estadoCivil);
+console.log(typeof arreglo);
+console.log(arreglo.length);
