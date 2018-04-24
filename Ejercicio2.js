2f8b9424c05c2fb77bcc484916574cda55ac6f8f
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-04-24 08:58:48 -0500

    4

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,2 +1,49 @@
 // CUASCOTA GABRIELA
 // desarrollo
+var gen = [ gen1=62,
+            gen2=62,
+            gen3=62,
+            gen4=62,
+            gen5=124,
+            gen6=124,
+            gen7=124,
+            gen8=124,
+            gen9=124,
+            gen10=124,
+            gen11=124,
+            gen12=124,
+            gen13=124,
+            gen14=124,
+            gen15=124,
+            gen16=124,
+            gen17=124,
+            gen18=124,
+            gen19=124]
+var A = 0;
+var total1 = 0;
+while (A < 4){
+    total1 = total1 + gen[A];
+    console.log('Generador #' + (A + 1) + ' está prendido, añadiendo ' + gen[A] + ' MW para un total de ' + total1 + ' MW');
+    A++;
+}
+var B = 4;
+for (B ; B<gen.length; B++) {
+    total1 = total1 + gen[B];
+    console.log('Generador #' + (B + 1) + ' está prendido, añadiendo ' + gen[B] + ' MW para un total de ' + total1 + ' MW.');
+}
+while (A < 4){
+    if((A + 1) % 2 == 0){
+        console.log('Generador # ' + (A + 1) + ' esta prendido, añadiendo ' + gen[A] + ' MW para un total de ' + total1 + ' MW.');
+    }
+    A++;
+    }
+
+for (B; B < gen.length; B++) {
+    if ((B + 1) % 2 === 0) {
+        console.log('Generador #' + (B + 1) + ' está Apagado');
+    }
+    else {
+        total1 = total1 + gen[B];
+        console.log('Generador # ' + (B + 1) + ' está prendido, añadiendo ' + gen[B] + ' MW para un total de: ' + total1 + ' MW.');
+    }
+}
